a010041ecd08dfc2b16bc2bb08d23d2ab8f634e8
Kaan <73829362+Kaan-Ince@users.noreply.github.com>
2025-07-24 18:18:17 +0200

    Update ml_project.py

--- ml_project.py
+++ ml_project.py
@@ -27,43 +27,49 @@ with st.expander("The Dataset After Initial Cleaning"):
     df
 
 
-with st.expander("The Dataset Post-Processing"):
-    
-    df["language_code"] = pd.factorize(df["language_code"])[0]
-    df["publication_date"] = pd.to_datetime(df["publication_date"]).dt.year
+df["language_code"] = pd.factorize(df["language_code"])[0]
+df["publication_date"] = pd.to_datetime(df["publication_date"]).dt.year
 
+df["occurence_count"] = df.groupby("title")["title"].transform("count")
 
-    df["occurence_count"] = df.groupby("title")["title"].transform("count")
+df["occ_weighted"] = df["average_rating"] * df["occurence_count"]
+df["rating_count_weighted"] = df["average_rating"] * df["ratings_count"]
+df["text_review_count_weighted"] = df["average_rating"] * df["text_reviews_count"]
+df["page_weighted"] = df["average_rating"] * df["num_pages"]
 
-    df["occ_weighted"] = df["average_rating"] * df["occurence_count"]
-    df["rating_count_weighted"] = df["average_rating"] * df["ratings_count"]
-    df["text_review_count_weighted"] = df["average_rating"] * df["text_reviews_count"]
-    df["page_weighted"] = df["average_rating"] * df["num_pages"]
-
-    df_processed = df.drop(columns=["title", "authors", "isbn", "isbn13", "publisher"])
-    
-    df_processed
+df_processed = df.drop(columns=["title", "authors", "isbn", "isbn13", "publisher"])
 
 
 X = df.drop("average_rating", axis=1)
-y = df["average_rating"]
+y = df_processed["average_rating"]
 
+X_processed = df_processed.drop("average_rating", axis=1)
 
 with st.sidebar:
     st.header("Input Book Title")
-    title = st.selectbox("Book Title", (df["title"]))
-    input_data = {"Title": title}
+    title = st.selectbox("Select Book", (df["title"]))
+    authors = df["authors"]
+    average_rating = df["average_rating"]
+    isbn = df["isbn"]
+    isbn13 = df["isbn13"]
+    language_code = df["language_code"]
+    num_pages = df["num_pages"]
+    ratings_count = df["ratings_count"]
+    text_reviews_count = df["text_reviews_count"]
+    publication_date = df["publication_date"]
+    publisher = df["publisher"]
+    
+    input_data = {"title": title, "authors": authors, "average_rating": average_rating,
+                  "isbn": isbn, "isbn13": isbn13, "language_code": language_code,
+                  "num_pages": num_pages, "ratings_count": ratings_count, "text_reviews_count": text_reviews_count,
+                  "publication_date": publication_date, "publisher": publisher}
+    input_df = pd.DataFrame(input_data, index=[0])
+    input_book = pd.concat([input_df, X], axis=0)
 
-with st.expander:
-    st.header("Inputted Book")
-    input_data
-
-
-input_row = df[:1]
-
+input_row = input_book[:1]
 
 random_forest_reg = RandomForestRegressor(random_state=42)
-random_forest_reg.fit(X, y)
+random_forest_reg.fit(X_processed, y)
 y_predicted = np.round(random_forest_reg.predict(input_row), 2)
 
 
